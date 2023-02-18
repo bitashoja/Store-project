99fb23523378e16adbc92d3e3406bc5840592b34
bita <shojabita@gmail.com>
2023-02-18 23:16:55 +0330

    update descrption

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -5,11 +5,7 @@ export default function Description() {
   return (
     <div className="description">
       <div className="description-img">
-        <img
-          src="./image/54547 1.svg"
-          className="menu-laptop"
-          alt="menu laptop"
-        />
+        <img src="./image/54547 1.svg" />
       </div>
       <div className="description-texts">
         <h4>What can we do for you at BEYOND IT ?</h4>
