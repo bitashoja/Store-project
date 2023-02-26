1510756eb349e3caf08ca6d0482fd569ee523e02
bita <shojabita@gmail.com>
2023-02-26 09:08:07 +0330

    added design

--- src/components/uIUX Design/Design.jsx
+++ src/components/uIUX Design/Design.jsx
@@ -4,29 +4,39 @@ import "./design.css";
 export default function Design() {
   return (
     <div className="design">
+      <div className="image-design" id="image-design-top">
+        <img src="./images/ui-mobile.svg" alt="ui" />
+      </div>
       <div className="text-design">
         <h3>UIUX Design</h3>
         <p>
           Don't you want to have a website that just exists? The effort of
-          Beyond specialists is to design a site for you that will make the most
-          of it. We always follow these principles Design according to the
-          standard of the day Analysis of competitors Design with PS/XD/figam
-          based on customer's requirement Compliance with the basic principles
-          of UX in design Compliance with the principles of color psychology No
-          limit on the number of corrections and edits User experience (UX) and
-          user interface (UI) are very decisive components in any digital
-          product. Your audience first judges the appearance of your product and
-          then decides to use it. A suitable UI can attract more audience to
-          your product and a good UX makes using your product enjoyable and
-          pleasant, thus making existing users stay, and attracting new users.
-          Therefore, a proper UI/UX design makes it easier to communicate with
-          users.
+          <span className="beond-span">
+            BEOND <img src="./images/Subtract.svg" alt="Subtract" />
+          </span>
+          specialists is to design a site for you that will make the most of it.
+          <ul>
+            <li>We always follow these principles</li>
+            <li>Design according to the standard of the day</li>
+            <li>Analysis of competitors</li>
+            <li>Design with PS/XD/figam based on customer's requirement</li>
+            <li>Compliance with the basic principles of UX in design</li>
+            <li>Compliance with the principles of color psychology</li>
+            <li>No limit on the number of corrections and edits</li>
+          </ul>
+          User experience (UX) and userinterface (UI) are very decisive
+          components in any digital product. Your audience first judges the
+          appearance of your product and then decides to use it. A suitable UI
+          can attract more audience to your product and a good UX makes using
+          your product enjoyable and pleasant, thus making existing users stay,
+          and attracting new users. Therefore, a proper UI/UX design makes it
+          easier to communicate with users.
         </p>
         <div className="button-design">
           <img src="./images/button-img.svg" alt="button" />
         </div>
       </div>
-      <div className="image-design">
+      <div className="image-design" id="image-design-left">
         <img src="./images/ui design.svg" alt="ui" />
       </div>
     </div>
