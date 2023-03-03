5443206a84a3ab1295e32cfe5988eff91ede57ea
hshoja <hamid.shoja@gmail.com>
2023-03-03 23:13:39 +0100

    Fix names

--- src/components/headerPortfolio/HeaderPortfolio.jsx
+++ src/components/headerPortfolio/HeaderPortfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../Header/Header'
+import Header from '../header/Header'
 import './headerPortfolio.css'
 
 export default function HeaderPortfolio() {
