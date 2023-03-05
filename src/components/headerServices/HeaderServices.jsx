5cb0d3399745bb0f22f551e33d030cf8b76eb85b
hshoja <hamid.shoja@gmail.com>
2023-03-05 11:15:00 +0100

    Fix header component

--- src/components/headerServices/HeaderServices.jsx
+++ src/components/headerServices/HeaderServices.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Header/Header";
+import Header from "../Header/HeaderComponent";
 import "./headerService.css";
 
 export default function HeaderServices() {
