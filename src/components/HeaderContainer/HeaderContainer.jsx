b6fffcd13b32736dddb229f1a3f8a3ca8fa278af
hshoja <hamid.shoja@gmail.com>
2023-04-05 17:28:36 +0200

    remove icon

--- src/components/HeaderContainer/HeaderContainer.jsx
+++ src/components/HeaderContainer/HeaderContainer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Header from "../Header/HeaderComponent";
-import "./headerContainer.css";
 import { BsTelephoneFill } from "react-icons/bs";
 import { IoIosArrowForward } from "react-icons/io";
-import { RxEnvelopeClosed } from "react-icons/rx";
 import { Link } from "react-router-dom";
+import "./headerContainer.css";
 
 export default function HeaderContainer() {
   return (
